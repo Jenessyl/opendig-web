ec9fe4da26b64ecb15d4775c0111f006637a1695
Matthew Vincent <mvincent@basecamp.com>
2022-07-29 23:14:22 +0300

    improved sorting for finds

--- app/controllers/finds_controller.rb
+++ app/controllers/finds_controller.rb
@@ -3,6 +3,6 @@ class FindsController < ApplicationController
     @area = params[:area_id]
     @square = params[:square_id]
     @finds = @db.view('opendig/finds_for_square', {reduce: false, start_key: ["#{@area}.#{@square}"], end_key:["#{@area}.#{@square}", {}] })["rows"].map{|row| Find.new(row["value"])}
-    @finds.sort_by!{|find| [-find.season, find.field_number]}
+    @finds.sort_by!{|find| [-find.season.to_i, find.field_number.to_i]}
   end
 end
