4e1ed0fca912cac5eb91a9d1a27b3b3529011da7
Paul Hartman <contact@paulhartman.dev>
2026-01-16 00:37:08 +0000

    snapshot: in process of implementing basic OmniAuth user authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def create
+    auth = request.env['omniauth.auth']
+    user = User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+      u.email = auth['info']['email']
+      u.name = auth['info']['name']
+    end
+    session[:user_id] = user.id
+    redirect_to root_path, notice: "Logged in!"
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get '/auth/:provider/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :areas, only: [:index, :new, :create] do
     resources :squares, only: [:index, :new, :create] do
       resources :pails, only: [:index]
