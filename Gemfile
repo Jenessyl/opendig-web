e826d11f005e9cb7617d7ac58e19b24ec3578389
Paul Hartman <contact@paulhartman.dev>
2026-01-16 00:37:08 +0000

    snapshot: in process of implementing basic OmniAuth user authentication

--- Gemfile
+++ Gemfile
@@ -85,3 +85,10 @@ group :test do
 end
 
 gem "tailwindcss-rails", "~> 2.0"
+
+# Omniauth for: Google, Github, Microsoft
+gem "omniauth"
+gem "omniauth-rails_csrf_protection"
+gem "omniauth-google-oauth2"
+gem "omniauth-github"
+gem "omniauth-microsoft-office365"
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,21 @@
+# OmniAuth.config.full_host = Rails.env.production? ? 'https://domain.com' : 'http://localhost:3000'
+# OmniAuth.config.allowed_request_methods = %i[get]
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+    scope: 'email,profile',
+    prompt: 'select_account',
+    image_aspect_ratio: 'square',
+    image_size: 50
+  }
+
+  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: "user:email"
+
+  provider :microsoft_office365, ENV['MICROSOFT_CLIENT_ID'], ENV['MICROSOFT_CLIENT_SECRET'], {
+    scope: 'openid User.Read'
+  }
+
+  if Rails.env.development?
+    provider :developer
+  end
+end
