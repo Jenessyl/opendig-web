b9e426a1f828c44f8f773f0d6aac961b2159e56d
Paul Hartman <contact@paulhartman.dev>
2026-02-26 00:48:37 +0000

    Fix `pending` marker for static content tests

--- spec/system/static_content_spec.rb
+++ spec/system/static_content_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe 'Static content', type: :system do
-  pending 'integration tests for static content pages' do
-    # TODO
-  end
+  pending 'integration tests for static content pages'
 end
